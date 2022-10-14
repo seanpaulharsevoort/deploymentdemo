3e2f1e06cc497f82c638ed72c5e872381d2a0e52
seanpaulharsevoort <spharsevoort@dynamicdevelopment.nl>
2022-10-14 09:05:32 +0200

    Add first unit test

---
+++ DeploymentDemo.Test/DikkeMikTest.cs
@@ -0,0 +1,16 @@
+namespace DeploymentDemo.Test
+{
+    public class DikkeMikTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void DikkeMIkTest1()
+        {
+            Assert.Pass();
+        }
+    }
+}
