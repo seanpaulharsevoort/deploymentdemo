182747e59b48aef76949c3234a781236b0530426
seanpaulharsevoort <spharsevoort@dynamicdevelopment.nl>
2022-10-14 12:18:28 +0200

    Add unittests

--- DeploymentDemo.Test/UnitTest1.cs
+++ DeploymentDemo.Test/UnitTest1.cs
@@ -23,28 +23,28 @@ public class Tests
         Assert.AreEqual(expected, actual);
     }
 
-    // [Test]
-    // public void Test2()
-    // {
-    //     var mikMak = new MikMak();
-    //     bool actual = mikMak.isTrue(true);
-    //     bool expected = false;
-    //     Assert.AreNotEqual(expected, actual);
-    // }
-    // [Test]
-    // public void Test3()
-    // {
-    //    var mikMak = new MikMak();
-    //     bool actual = mikMak.isTrue(true);
-    //     bool expected = false;
-    //     Assert.AreNotEqual(expected, actual);
-    // }
-    // [Test]
-    // public void Test4()
-    // {
-    //     var mikMak = new MikMak();
-    //     bool actual = mikMak.isTrue(true);
-    //     bool expected = true;
-    //     Assert.AreEqual(expected, actual);
-    // }
+    [Test]
+    public void Test2()
+    {
+        var mikMak = new MikMak();
+        bool actual = mikMak.isTrue(true);
+        bool expected = false;
+        Assert.AreNotEqual(expected, actual);
+    }
+    [Test]
+    public void Test3()
+    {
+       var mikMak = new MikMak();
+        bool actual = mikMak.isTrue(true);
+        bool expected = false;
+        Assert.AreNotEqual(expected, actual);
+    }
+    [Test]
+    public void Test4()
+    {
+        var mikMak = new MikMak();
+        bool actual = mikMak.isTrue(true);
+        bool expected = true;
+        Assert.AreEqual(expected, actual);
+    }
 }
